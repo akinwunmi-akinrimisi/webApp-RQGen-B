fb9c184a38853053d0b91246dc5df83d6b2ce4c8
akinwunmi-akinrimisi <akinolaakinrimisi@gmail.com>
2023-06-29 23:45:26 +0100

    modified server.js file

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express");
 require("dotenv").config();
 
 const app = express();
-const PORT = parseInt(process.env.PORT, 10); // Set server port
 
 app.use(express.static("dist"));
+
+// NGINX uses the PORT environment variable directly, no need to parse it
+const PORT = process.env.PORT || 3000; // Set server port
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
